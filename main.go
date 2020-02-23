bec24aafd4268c3255c3ebc88d746b41af01dbda
hongdongsu <dsh@storecamera.io>
2020-02-23 19:53:53 +0900

    feat: get request

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
+
+func ping(context *gin.Context) {
+	context.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
 
 func main() {
-	fmt.Println("We good")
+	r := gin.Default()
+
+	r.GET("/ping", ping)
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
